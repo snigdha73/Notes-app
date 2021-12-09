56b56dbad7a836c037b8588be3c484ca5e5fc82d
Snigdha <snigdha73.2000@gmail.com>
2021-12-10 03:08:59 +0530

    mongodb atlas

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ const app=express();
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/Notes",{useNewUrlParser:true,  useUnifiedTopology: true,
+mongoose.connect("mongodb+srv://Admin:Admin@cluster0.mq7cb.mongodb.net/todolistDB",{useNewUrlParser:true,  useUnifiedTopology: true,
 useFindAndModify: false});
-//mongodb+srv://Admin:Admin@cluster0.mq7cb.mongodb.net/todolistDB
 const itemsSchema = {
     name: String
 };
